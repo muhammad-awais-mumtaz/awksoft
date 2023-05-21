0c04e3b34fda49e6989c2a5ddc98e242fe9f0651
Awais <muhammadawaismumtaz2786@gmail.com>
2023-05-22 02:58:08 +0500

    added hero section in home page

---
+++ components/hero/hero.tsx
@@ -0,0 +1,34 @@
+import styles from "./hero.module.css";
+
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <>
+      <div className={styles.main}>
+        <section className={styles.details}>
+          <h1>
+            Custom Teams for Your Project: Streamline Your Workflow and Save
+            Costs!
+          </h1>
+          <p>
+            At our company, we offer tailored teams to suit your specific
+            project needs. Whether you are experiencing challenges with
+            development, marketing, or design, our custom teams are available to
+            reduce the time and costs associated with your project.
+          </p>
+        </section>
+        <section className={styles.illustration}>
+          <div>
+            <Image
+              src="/hero/img.png"
+              alt="My Image"
+              width={340}
+              height={340}
+            />
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
