75c9c6911404f8f707478ac2f33502cbad163d9c
muhammad-awais-mumtaz <muhammadawaismumtaz2786@gmail.com>
2023-08-10 08:14:59 +0500

    added social links to footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,10 +1,28 @@
+import Link from "next/link";
 import styles from "./footer.module.css";
+import { BsTwitter } from "react-icons/bs";
+import { FaFacebook } from "react-icons/fa";
+import { AiOutlineCopyright } from "react-icons/ai";
 
 export default function Footer() {
   return (
     <>
       <div className={styles.cont}>
-        <section>I am Footer</section>
+        <section className={styles.linksCont}>
+          <Link href={"https://twitter.com/_awksoft"}>
+            twitter <BsTwitter />
+          </Link>
+          <Link
+            href={"https://web.facebook.com/people/Awksoft/100094570092780/"}
+          >
+            facebook <FaFacebook />
+          </Link>
+        </section>
+        <section className={styles.refCont}>
+          <div>
+            <AiOutlineCopyright /> at Awksoft
+          </div>
+        </section>
       </div>
     </>
   );
