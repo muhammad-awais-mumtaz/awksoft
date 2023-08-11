378e12298b23d2a5638ca749f9444a9ef9e01ab7
muhammad-awais-mumtaz <muhammadawaismumtaz2786@gmail.com>
2023-08-11 06:49:57 +0500

    added about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,8 +7,17 @@ export const metadata: Metadata = {
 
 export default function About() {
   return (
-    <>
-      <div>hello iam about</div>
-    </>
+    <div className={styles.cont}>
+      <div className={styles.photo}>
+        <div className={styles.imgCont}></div>
+      </div>
+      <h1 className={styles.heading}>About Awksoft</h1>
+      <p className={styles.para}>
+        Hello! We're Awksoft. We have special teams that can make websites,
+        create nice designs, and help tell others about your business. We're in
+        Islamabad, Pakistan. We use new tricks to make your business even
+        better.
+      </p>
+    </div>
   );
 }
